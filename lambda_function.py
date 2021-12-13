7fb44b8b2b443b68f106654fc99e1c3876537e5c
lucaseliel <lucaseliel.business@gmail.com>
2021-12-13 17:37:36 -0300

    Ajuste lib Azure

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,3 @@ from src.controller.ocr_cupom_controller import OcrCupomCommandController
 
 def lambda_handler(event, context):
     return OcrCupomCommandController(event).invoke()
-
-
-lambda_handler({"resource": "/cupons-invalidos", "httpMethod": "GET"}, {})
